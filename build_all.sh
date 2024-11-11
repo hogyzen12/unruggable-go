0c9b0cc3d5e851005b60558db79764fe4855ddeb
hogyzen12 <billypapas12@gmail.com>
2024-11-11 18:07:52 +0100

    testing

--- build_all.sh
+++ build_all.sh
@@ -3,3 +3,6 @@ fyne package -os darwin -name Unruggable.app -appID com.unruggable.app -icon ico
 fyne package -os ios -name Unruggable.app -appID com.unruggable.app -icon icon.png
 
 fyne package -os iossimulator -appID com.unruggable.app -icon icon.png
+fyne package -os windows -name Unruggable.apk -appID com.unruggable.app -icon icon.png
+
+fyne release -os android -name Unruggable.apk -appID com.unruggable.app -icon icon.png -appVersion 1.0 -appBuild 1 -category utilities
